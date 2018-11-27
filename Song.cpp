426806cc874d7a0ede0e5c0f4b3eb05a50372ba5
averma1 <anika_verma@yahoo.com>
2018-11-27 09:46:43 -0500

    initial commit of song .cpp and .h file

---
+++ Song.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Anika on 11/20/18.
+//
+
+#include "Song.h"
+
+Song::Song(){
+
+    playCount = 0;
+
+}
+
+void addToPlayCount(int playCount){
+
+    playCount++;
+
+}
---
+++ Song.h
@@ -0,0 +1,25 @@
+//
+// Created by Anika on 11/20/18.
+//
+
+#ifndef TERMPROJECT_SONG_H
+#define TERMPROJECT_SONG_H
+
+//#include "ArrayList.h"
+#include<iostream>
+
+class Song{
+
+private:
+    std::string artist;
+    std::string title;
+    double duration;
+    int playCount; //number of times we played the song
+
+public:
+    void addToPlayCount(int playCount);
+
+};
+
+
+#endif //TERMPROJECT_SONG_H
