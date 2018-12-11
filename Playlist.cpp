bbdbea4ea80b11305afcf3e8620eeafde5487b21
mnoel33 <mnoel@ithaca.edu>
2018-12-11 10:17:10 -0500

    playlist

--- Playlist.cpp
+++ Playlist.cpp
@@ -48,11 +48,15 @@ void Playlist::addSong(Song* songToAdd){
 
 void Playlist::playNext(){
     duration-=songList->getValueAt(0)->getDuration();
-    songList->removeValueAtFront();
+    Song*songPlayed= songList->removeValueAtFront();
+    songPlayed->addToPlayCount(1);
 }
 
 void Playlist::removeSong(Song* songToRemove){
     int index=songList->find(songToRemove);
+    if(index==-1){
+        throw std::invalid_argument("song not in list");
+    }
     Song* songRemoved=songList->removeValueAt(index);
     duration-=songRemoved->getDuration();
 }
